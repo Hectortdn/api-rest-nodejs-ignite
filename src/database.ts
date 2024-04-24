de23d4c8f1b2a8aaa0091a1eb35cbb4ed1a71f16
Hectortdn <hector.kayque012@gmail.com>
2024-04-24 08:34:17 -0300

    chose: config postgree

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,15 @@
 import { Knex, knex as setupKnex } from "knex";
 import { env } from "./env";
 
-export const config: Knex.Config = {
-  client: "sqlite",
-  connection: {
-    filename: env.DATABASE_URL,
-  },
-  useNullAsDefault: true,
+const connectionDatabase = {
+  sqlite: { filename: env.DATABASE_URL },
+  pg: env.DATABASE_URL,
+};
 
+export const config: Knex.Config = {
+  useNullAsDefault: true,
+  client: env.DATABASE_CLIENT,
+  connection: connectionDatabase[env.DATABASE_CLIENT],
   migrations: {
     extension: "ts",
     directory: "./db/migrations",
